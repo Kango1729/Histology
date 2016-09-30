14d00b1aa2ba691faecaa94e12f59a9d3e50c315
Kango1729_PC <kango.kawase@gmail.com>
2016-10-01 03:03:55 +0900

    add python file for txt -> html

---
+++ htmlHistology/txt2html.py
@@ -0,0 +1,31 @@
+#coding:utf-8
+
+txtName = input('txtFile Name?: ') + ".txt"
+txtFile = open(txtName, 'r',encoding = 'utf-8')
+txtLines = txtFile.readlines()
+txtFile.close()
+OutputText = []
+for txtLine in txtLines:
+    tags = txtLine.split("#")
+    for i in range(1, len(tags), 2):
+        datasets = tags[i].split(",")
+        tags[i] = "<span class = 'form' data-answer = '" + datasets[0] + "' data-eng ='" + datasets[1] + "' data-hide = '" + datasets[2] + "' data-link = 'Text/Dict/" + datasets[1] + "'>  </span>"
+    OutputText.append("".join(tags))
+
+templateName = "Template.html" #input('templateName?: ' )
+templateFile = open(templateName, 'r',encoding = 'utf-8')
+templateLines = "".join(templateFile.readlines())
+templateFile.close()
+
+
+templateFront = templateLines[:templateLines.find("<span")]
+templateBack = templateLines[templateLines.find("</span>") + 7:]
+
+OutputLine = templateFront + "".join(OutputText) + templateBack
+print(OutputLine)
+
+htmlName = input('htmlFile Name?: ') + ".html"
+htmlFile = open(htmlName, 'w', encoding = 'utf-8')
+htmlFile.write(OutputLine)
+htmlFile.fluch()
+htmlFile.close()
