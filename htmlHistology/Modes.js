af5f915100b35c7279353e32f86804a1dca0a178
Kango1729_PC <kango.kawase@gmail.com>
2016-10-02 23:39:26 +0900

    Better ReadMode with changes in base reference

--- htmlHistology/Modes.js
+++ htmlHistology/Modes.js
@@ -1,18 +1,41 @@
 function ReadMode(form){
     [].forEach.call(document.getElementsByClassName(form),function(form){
-        switch (Number(document.getElementById("ReadLang").value)) {
-            case 1:
-                form.innerHTML = "<b>" + form.dataset.jp + "</b>"
-                break;
-            case 2:
-                form.innerHTML = "<b>" + form.dataset.eng + "</b>"
-                break;
-            default:
-            form.innerHTML = "<b>" + form.dataset.jp + " &quot;" +form.dataset.eng + "&quot; </b>"
 
-        }
+        form.innerHTML = ReadForm(form, '#000000' )
+
     });
 }
+function ReadForm(form, Color){
+    switch (Number(document.getElementById("ReadLang").value)) {
+        case 1:
+            output = "<font color = " + Color + ">" + form.dataset.jp + "</font>";
+            break;
+        case 2:
+            output = "<font color = " + Color + ">" + form.dataset.eng + "</font>"
+            break;
+        default:
+            output = "<font color = " + Color + ">" + form.dataset.jp + "&quot;" +  form.dataset.eng + "</font>&quot;";
+        }
+        var boolBold = {"0":1,"1":1,"2":0,"3":0,"4":0,"topic":3,"origin":2};
+        var a = form.dataset.rfont
+        switch (boolBold[form.dataset.rfont]) {
+            case 0:
+                output = output;
+                break;
+            case 1:
+                output = "<b>" + output + "</b>";
+                break;
+            case 2:
+                output = "<i>" + output + "</i>"
+                break;
+            case 3:
+                output = "<b><i>" + output + "</i></b>"
+            default:
+        }
+
+return output;
+}
+
 function QuestMode (form){
     [].forEach.call(document.getElementsByClassName(form), function(form){
         form.innerHTML =""
@@ -28,35 +51,35 @@ function QuestMode (form){
         (Questlang) {
             case 0:
             var ques = ""
-            var ans = form.dataset.jp;
+            var cor = form.dataset.jp;
             break;
             case 1:
             var ques = ""
-            var ans = form.dataset.eng;
+            var cor = form.dataset.eng;
             break;
             case 2:
             var ques = form.dataset.jp;
-            var ans = form.dataset.eng;
+            var cor = form.dataset.eng;
             break;
             case 3:
             var ques = form.dataset.eng;
-            var ans = form.dataset.jp;
+            var cor = form.dataset.jp;
             break;
         }
 
         button.addEventListener("click", function(){
-            check(ans, form, text);
+            check(cor, form, text.value);
         });
         form.innerHTML = "<b>"  +  ques + "</b>"
         form.insertBefore(text, null);
         form.insertBefore(button, null);
     });
 }
-function check(answer, form, text){
 
-    if(text.value == answer){
-        form.innerHTML = "<font color = '#ff4e4e'><b>" + text.value + "<b></font>";
+function check(correct, form, answer){
+    if(answer == correct){
+        form.innerHTML = ReadForm(form, '#ff4e4e' );
     } else {
-        form.innerHTML = "<font color = '#ff4e4e'><b>" + answer + "<b></font>" + "   <font color = '#0003ff'><b>not(" + text.value + ")<b></font>";
+        form.innerHTML = ReadForm(form, '#ff4e4e' ) + "<font color = '0003ff'><b>×(" + answer + ")<b></font>";
     }
 }
