d42f01c40ef81eac90c7113390aeca3174e808c6
Kango_iPad <kango.kawase@gmail.com>
2016-10-03 00:57:59 +0900

    add boolHide and others

--- htmlHistology/Modes.js
+++ htmlHistology/Modes.js
@@ -14,10 +14,9 @@ function ReadForm(form, Color){
             output = "<font color = " + Color + ">" + form.dataset.eng + "</font>"
             break;
         default:
-            output = "<font color = " + Color + ">" + form.dataset.jp + "&quot;" +  form.dataset.eng + "</font>&quot;";
+            output = "<font color = " + Color + ">" + form.dataset.jp + "&quot;" +  form.dataset.eng + "&quot;</font>";
         }
         var boolBold = {"0":1,"1":1,"2":0,"3":0,"4":0,"topic":3,"origin":2};
-        var a = form.dataset.rfont
         switch (boolBold[form.dataset.rfont]) {
             case 0:
                 output = output;
@@ -29,7 +28,7 @@ function ReadForm(form, Color){
                 output = "<i>" + output + "</i>"
                 break;
             case 3:
-                output = "<b><i>" + output + "</i></b>"
+                output = "<font color = '#ff0000'><b>" + output + "</b></font>"
             default:
         }
 
@@ -38,41 +37,42 @@ return output;
 
 function QuestMode (form){
     [].forEach.call(document.getElementsByClassName(form), function(form){
-        form.innerHTML =""
-        var text = document.createElement("input");
-        text.type = "text";
-        text.size = "8"
+    	var boolHide = {"0":0,"1":1,"2":0,"3":1,"4":0,"topic":0,"origin":0};
+    	if (boolHide[form.dataset.rfont] == 1){
+        	form.innerHTML =""
+        	var text = document.createElement("input");
+        	text.type = "text";
+        	text.size = "8"
 
-        var button = document.createElement("input");
-        button.type = "button";
-        button.value = " ";
-        Questlang = Number(document.getElementById("QuestLang").value)
-        switch
-        (Questlang) {
-            case 0:
-            var ques = ""
-            var cor = form.dataset.jp;
-            break;
-            case 1:
-            var ques = ""
-            var cor = form.dataset.eng;
-            break;
-            case 2:
-            var ques = form.dataset.jp;
-            var cor = form.dataset.eng;
-            break;
-            case 3:
-            var ques = form.dataset.eng;
-            var cor = form.dataset.jp;
-            break;
+        	var button = document.createElement("input");
+        	button.type = "button";
+        	button.value = " ";
+        	Questlang = Number(document.getElementById("QuestLang").value)
+        	switch(Questlang) {
+            	case 0:
+            		var ques = ""
+            		var cor = form.dataset.jp;
+            	break;
+            	case 1:
+            		var ques = ""
+            		var cor = form.dataset.eng;
+            	break;
+            	case 2:
+            		var ques = form.dataset.jp;
+            		var cor = form.dataset.eng;
+            	break;
+            	case 3:
+            		var ques = form.dataset.eng;
+            		var cor = form.dataset.jp;
+            	break;
+        	}
+
+        	button.addEventListener("click",function(){check(cor, form, text.value);
+        	});
+        	form.innerHTML = "<b>"  +  ques + "</b>"
+        	form.insertBefore(text, null);
+        	form.insertBefore(button, null);
         }
-
-        button.addEventListener("click", function(){
-            check(cor, form, text.value);
-        });
-        form.innerHTML = "<b>"  +  ques + "</b>"
-        form.insertBefore(text, null);
-        form.insertBefore(button, null);
     });
 }
 
@@ -80,6 +80,6 @@ function check(correct, form, answer){
     if(answer == correct){
         form.innerHTML = ReadForm(form, '#ff4e4e' );
     } else {
-        form.innerHTML = ReadForm(form, '#ff4e4e' ) + "<font color = '0003ff'><b>×(" + answer + ")<b></font>";
+        form.innerHTML = ReadForm(form, '#ff4e4e' ) + "<font color = '0003ff'><b>  ×(" + answer + ")<b></font>";
     }
 }
